d95c1dffe242eef60b7f75f7357f4740b84b24be
Anthony4Dev <anthony.islan2005@gmail.com>
2023-07-02 18:42:54 -0300

    adicionando os numeros hexadecimais para substituir pelo que foi digitado

---
+++ src/password-generator.py
@@ -0,0 +1,41 @@
+
+"""
+Security = chave
+5ecur1ty = senha
+
+hex
+
+1=1
+2=2
+3=3
+4=4
+5=5
+6=6
+7=7
+8=8
+9=9
+10=A
+11=B
+12=C
+13=D
+14=E
+15=F
+
+"""
+
+chave = input("Digite como você quer a sua senha: ")
+
+senha = ""
+for letra in chave:
+    if letra in "Aa":
+        senha = senha + "10"
+    elif letra in "Bb":
+        senha = senha + "11"
+    elif letra in "Cc":
+        senha = senha + "12"
+    elif letra in "Dd":
+        senha = senha + "13"
+    elif letra in "Ee":
+        senha = senha + "14"
+    elif letra in "Ff":
+        senha = senha + "15"
